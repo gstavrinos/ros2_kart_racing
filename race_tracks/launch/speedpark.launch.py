c343b069067b65c5b340cbcb71ef1656238f7378
George Stavrinos <gstavrinos@protonmail.com>
2022-10-11 17:52:34 +0300

    Added the speedpark race track

---
+++ race_tracks/launch/speedpark.launch.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="race_tracks",
+            description="The package that includes the track description.",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="speedpark.urdf.xacro",
+            description="The track description urdf.",
+        )
+    )
+
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+
+    robot_description_content = Command(
+         [
+             PathJoinSubstitution([FindExecutable(name="xacro")]),
+             " ",
+             PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
+             ]
+    )
+
+    return LaunchDescription(
+        declared_arguments + 
+        [
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="track_state_publisher",
+            output="screen",
+            parameters=[{"use_sim_time": True, "robot_description": robot_description_content, "publish_frequency": 20.0, "ignore_timestamp": False,
+            "frame_prefix":"",}],
+            remappings=[("/robot_description","/track_description"
+            )]),
+
+        Node(
+            package="joint_state_publisher",
+            executable="joint_state_publisher",
+            name="track_joint_state_publisher",
+            remappings=[("/robot_description","/track_description")],
+            parameters=[{"use_sim_time": True,}],
+            output="screen")
+        ]
+    )
